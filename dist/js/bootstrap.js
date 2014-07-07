57ded1754d205efa49216b63f22394e2d732d278
Scott <scott@powershift.tv>
2014-07-07 09:04:57 +0100

    Update generated files

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -67,6 +67,35 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
 }(jQuery);
 
 /* ========================================================================
+ * Bootstrap: touch-detection.js v3.1.1
+ * http://getbootstrap.com/
+ * ========================================================================
+ * Copyright 2014 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
+/*! Credit: Modernizr v3.0.0-alpha.0 | MIT */
+
+
++function ($) {
+  'use strict';
+
+  $(function () {
+    // Based on: https://github.com/Modernizr/Modernizr/blob/master/feature-detects/touchevents.js
+    // and http://www.w3.org/TR/pointerevents/#examples
+
+    var testDiv, isTouch = ('ontouchstart' in window) || (window.navigator.pointerEnabled && window.navigator.maxTouchPoints > 0)
+    if (!isTouch) {
+      testDiv = $('<div class="bs-touch-test"></div>').appendTo(document.body)
+      isTouch = testDiv.css('top') == '-99px'
+      testDiv.remove()
+    }
+    if (!isTouch) {
+      (document.documentElement.className += ' bs-no-touch')
+    }
+  })
+}(jQuery);
+
+/* ========================================================================
  * Bootstrap: alert.js v3.2.0
  * http://getbootstrap.com/javascript/#alerts
  * ========================================================================
